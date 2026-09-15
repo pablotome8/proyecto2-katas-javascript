645d99350525dab3d30eff096b50fc05dd972125
Pablo Tome Manzano <pablotomemanzano2000@gmail.com>
2026-09-15 22:22:14 +0200

    Primer ejercicios

---
+++ ejercicios-basicos-avanzados/ejercicio-01.js
@@ -0,0 +1,17 @@
+// Define tu superheroe favorito
+let miSuperHeroeFavorito = "Hulk";
+
+//Define un numero
+let number = 50;
+
+//Define las dimensiones 
+let altura = 5;
+let longitud =10;
+
+//Calcula la suma
+let suma = altura + longitud;
+
+//Imprime los resultados 
+console.log(`Mi superheroe favorito ${miSuperHeroeFavorito} , el numero que he definido es ${number} , las dimesiones son ${altura} y ${longitud} que sumados dan un total de ${suma}`); 
+
+    
