25a9813891aeb3041654543d589e9466c7fad7cd
Pablo Tome Manzano <pablotomemanzano2000@gmail.com>
2026-09-16 14:36:47 +0200

    Ejercicios Resueltos

---
+++ ejercicios-basicos-avanzados/ejercicio-07.js
@@ -0,0 +1,6 @@
+// Completa esta función para que, al recibir dos números por argumento, te devuelva por consola el más alto de los dos.
+function greaterNumber(numberOne , numberTwo) {
+    console.log(Math.max(numberOne,numberTwo))
+}
+
+greaterNumber(33,7)
